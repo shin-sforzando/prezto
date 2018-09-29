c5ea4084a60bff70b60124b6e86075a4c4918758
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2018-09-29 16:05:39 +0900

    :+1: Add some defaults command in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,17 @@
 
 set -eux
 
+## Don't create .DS_Store on Network Drive
+defaults write com.apple.desktopservices DSDontWriteNetworkStores True
+killall Finder
+
+## Recent Applications in Dock
+defaults write com.apple.dock persistent-others -array-add '{ "tile-data" = { "list-type" = 1; }; "tile-type" = "recents-tile"; }'
+killall Dock
+
+## neovim
+pip3 install neovim
+
 ## nodebrew
 /usr/local/opt/nodebrew/bin/nodebrew setup_dirs
 nodebrew install-binary stable
